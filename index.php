794d319cee58c8160ce13a3c606197317ee16580
Sanira Deneth <sanira.adesha@gmail.com>
2025-06-05 19:35:08 +0530

    Created root.css file and defined color palette.

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php
-include("includes/db.php");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookNest</title>
+    <link rel="stylesheet" href="assets/css/root.css">
+</head>
+<body>
+    
+</body>
+</html>
