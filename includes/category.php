fb7dc7999989055b439f7924437b0191e9e08794
Avishka Medagamagodage <aimedagamagodage2003@gmail.com>
2025-06-12 10:09:58 +0530

    Created Category section, and add did some changes in the project

---
+++ includes/category.php
@@ -0,0 +1,62 @@
+<?php
+include("navbar.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookNest</title>
+    <link rel="stylesheet" href="../assets/css/root.css" />
+    <link rel="stylesheet" href="..//assets//css//category.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+</head>
+
+<body>
+    <div class="category-main page">
+        <h1>Browse by Category</h1>
+        <div class="category">
+            <div class="category-list">
+                <a><i class="fas fa-book"></i></a>
+                <h3>Classic</h3>
+                <p>2 Books</p>
+            </div>
+
+            <div class="category-list">
+                <a><i class="fas fa-book"></i></a>
+                <h3>Fantacy</h3>
+                <p>2 Books</p>
+            </div>
+
+            <div class="category-list">
+                <a><i class="fas fa-book"></i></a>
+                <h3>Self-Help</h3>
+                <p>2 Books</p>
+            </div>
+
+            <div class="category-list">
+                <a><i class="fas fa-book"></i></a>
+                <h3>Psychology</h3>
+                <p>2 Books</p>
+            </div>
+
+            <div class="category-list">
+                <a><i class="fas fa-book"></i></a>
+                <h3>Fiction</h3>
+                <p>2 Books</p>
+            </div>
+        </div>
+        <input type="button">View All Catogories</input>
+
+    </div>
+
+
+
+</body>
+
+</html>
+
+<?php
+include("footer.php");
+?>
